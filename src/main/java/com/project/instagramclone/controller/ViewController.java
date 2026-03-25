e6bbabced68cb4e87166354f9f44e465ffeb4903
kiyongee2 <kiyongee2@gmail.com>
2026-03-25 13:04:56 +0900

    번거로웠던 민수가 minsu_index 깃허브에 임시 저장하고 집가서 해야지~!

--- src/main/java/com/project/instagramclone/controller/ViewController.java
+++ src/main/java/com/project/instagramclone/controller/ViewController.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class ViewController {
-
+    // 민수가 회원가입 맡아서 매번 회원가입사이트 들어가서  개발 너무 귀찮다...
+    // 본인이 하는 회원가입 페이지를 / 임시로 변경하고 해야징~!
     @GetMapping("/")
     public String indexView() {
-        return "index";
+        return "minsu_index";
     }
     @GetMapping("/user/login")
     public String loginView() {
